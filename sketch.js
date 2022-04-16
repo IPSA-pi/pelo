a3dd17ee3efe9cca8115e8ae5c8a80a6a6b1fadd
unknown <sebeliusancira@gmail.com>
2022-04-15 20:43:20 -0700

    pelos tratando de convertirse en círculos

--- sketch.js
+++ sketch.js
@@ -1,5 +1,6 @@
 function setup() {
     createCanvas(600, 600);
+    // frameRate(1);
 }
 
 function draw() {
@@ -7,12 +8,15 @@ function draw() {
     translate(width/2, height/2);
     stroke(255);
     noFill();
+    let t = 0;
     beginShape();
-    for (let a = 0; a < TWO_PI; a += 0.01) {
-        let r = random(50, 100);
+    for (let a = 0; a < TWO_PI; a += 0.1) {
+        let r = noise(t) * 100;
         let x = r * cos(a);
         let y = r * sin(a);
         vertex(x,y);
+        t += .01;
     }
     endShape();
+    noLoop();
 }
