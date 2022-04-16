15e8a104fea1f6170c20263c931ac0bb19170228
unknown <sebeliusancira@gmail.com>
2022-04-15 21:00:15 -0700

    bola

--- sketch.js
+++ sketch.js
@@ -8,15 +8,15 @@ function draw() {
     translate(width/2, height/2);
     stroke(255);
     noFill();
-    let t = 0;
     beginShape();
     for (let a = 0; a < TWO_PI; a += 0.1) {
-        let r = map(noise(t),0,1,100,200); 
+        let xoff = map(cos(a), -1, 1, 0, 2);
+        let yoff = map(sin(a), -1, 1, 0, 2);
+        let r = map(noise(xoff, yoff),0,1,100,200); 
         let x = r * cos(a);
         let y = r * sin(a);
         vertex(x,y);
-        t += .1;
     }
-    endShape();
+    endShape(CLOSE);
     noLoop();
 }
